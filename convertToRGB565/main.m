a38f1599046ea70a683ab47f18092c91dc48bc4b
ジャスティン　フレシャワー <nekotron@warkstationtsu.home>
2021-04-11 17:36:22 -0700

    Initial Commit

---
+++ convertToRGB565/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  convertToRGB565
+//
+//  Created by ジャスティン　フレシャワー on 2021/04/11.
+//  Copyright © 2021年 ジャスティン　フレシャワー. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
